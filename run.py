52fafea2877839e30d9fae6ed1c788ed567edde5
LinusGordon <linusgordon@gmail.com>
2017-11-20 20:00:04 -0500

    Detect plagiarism

--- run.py
+++ run.py
@@ -8,22 +8,25 @@ def main():
 
     parser.add_argument(
         '-f', '--files', required=True, nargs=2,
-        help='Two files to compare for plagiarism'
+        help='Two files to compare for plagiarism',
     )
 
-    parser.add_argument('-s', '--synonyms', required=True, help='Synonyms file')
+    parser.add_argument('-s', '--synonyms', required=True, help='Synonyms file',)
 
     parser.add_argument(
-        '-t', '--tuple_size', type=int, help='Size of the tuple', default=PlagiarismDetector.DEFAULT_TUPLE_SIZE
+        '-t', '--tuple_size', type=int, help='Size of the tuple',
+        default=PlagiarismDetector.DEFAULT_TUPLE_SIZE,
     )
 
     parser = parser.parse_args()
 
-    files = parser.files
-    synonyms = parser.synonyms
-    tuple_size = parser.tuple_size
-
-    print(PlagiarismDetector(files, synonyms, tuple_size).get_percent_plagiarised())
+    print(
+        PlagiarismDetector(
+            parser.files,
+            parser.synonyms,
+            parser.tuple_size
+        ).get_percentage_plagiarized()
+    )
 
 
 if __name__ == '__main__':
